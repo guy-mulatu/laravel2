c077ce275a0b8e7f2d3a8b71c703724221c8a6a2
Guy Mulatu <guy.mulatu@crowdproperty.com>
2018-05-22 13:20:54 +0100

    no message

---
+++ resources/views/tasks/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Guy Mulatu
+ * Date: 22/05/2018
+ * Time: 12:59
+ */
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,44 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Welcome Page</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="cover.css" rel="stylesheet">
+</head>
+
+<body class="text-center">
+
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <header class="masthead mb-auto">
+        <div class="inner">
+            <nav class="nav nav-masthead justify-content-center">
+                <a class="nav-link active" href="#">Log in</a>
+                <a class="nav-link" href="#">Register</a>
+                <a class="nav-link" href={{'home'}}>Home</a>
+            </nav>
+        </div>
+    </header>
+
+    <main role="main" class="inner cover">
+        <h1 class="cover-heading">Welcome</h1>
+        <p class="lead">This app allows his users to follow up and update their attributed tasks</p>
+        <p class="lead">
+            <a href="#" class="btn btn-lg btn-secondary">Log in</a>
+        </p>
+    </main>
+
+</div>
+
+</body>
+</html>
